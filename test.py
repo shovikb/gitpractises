29f1f5346772d352b26c3125e4fa992db7bdc18a
shovikb <57484618+shovikb@users.noreply.github.com>
2023-07-24 20:04:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
 ## Created Test.py
-<<<<<<< HEAD
-=======
-
->>>>>>> 6488b87fc99fa0e1083ecd82ceb6304ca0a0ac29
 def addition(a,b,c):
+    print(a,b,c)
     return a+b+c
 
 def multiply(a,b):
-<<<<<<< HEAD
     return a*b
-=======
-    return a*b
->>>>>>> 6488b87fc99fa0e1083ecd82ceb6304ca0a0ac29
+
+
